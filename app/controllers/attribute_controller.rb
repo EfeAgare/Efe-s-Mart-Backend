69eeadbcfcbd1ac10a670fcb786f99f025458adc
Efe <knowledgeagare157@gmail.com>
2019-10-30 22:02:06 +0100

    get a single attribute

--- app/controllers/attribute_controller.rb
+++ app/controllers/attribute_controller.rb
@@ -15,7 +15,12 @@ class AttributeController < ApplicationController
 
   # get a single attribute using the attribute_id in the request parameter
   def get_single_attribute
-    json_response({ message: 'NOT IMPLEMENTED' })
+    single_attribute = Attribute.find(params[:attribute_id])
+    if single_attribute
+      json_response(single_attribute)
+    else
+      json_response({message: "Attribute not found"}, 404)
+    end
   end
 
   # get all attribute values of a single attribute using the attribute id
