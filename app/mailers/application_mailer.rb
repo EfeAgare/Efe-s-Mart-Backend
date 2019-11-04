ac6e79fa4ec6a514ad481776766e3cff78dd3611
Efe <knowledgeagare157@gmail.com>
2019-11-04 16:50:47 +0100

    add the mailer controller

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'Efe-T-Shirt <no-reply@efe.agare.com>'
   layout 'mailer'
 end
---
+++ app/mailers/customer_mailer.rb
@@ -0,0 +1,8 @@
+class CustomerMailer < ApplicationMailer
+
+  def payment_email
+    @user = params[:user]
+    @url  = 'http://example.com/login'
+    mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+  end
+end
