c9a4e029c1ce0e6e8f02c1eb52129287bee23353
Efe <knowledgeagare157@gmail.com>
2019-11-11 14:37:41 +0100

    create shopping cart factory

---
+++ spec/factories/shopping_cart_factory.rb
@@ -0,0 +1,19 @@
+FactoryBot.define do
+  factory :shopping_cart do
+    item_id
+      
+    cart_id
+      
+    product_id
+      
+    attribute_value
+      
+    quantity
+     
+    buy_now
+      
+    
+    added_on
+    
+  end
+end
