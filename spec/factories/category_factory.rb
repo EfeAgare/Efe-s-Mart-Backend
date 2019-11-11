2d1d5709cc11e436be2975eada590959a7c62c23
Efe <knowledgeagare157@gmail.com>
2019-11-11 14:03:39 +0100

    create customer factory

--- spec/factories/category_factory.rb
+++ spec/factories/category_factory.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :category do
     department
+    product
     name "nice"
     description "complete"
   end
---
+++ spec/factories/customer_factory.rb
@@ -0,0 +1,17 @@
+FactoryBot.define do
+  factory :customer do
+    name { "knowledge" }
+    email { "efe.love@gmail.com"}
+    password 
+    credit_card
+    address_1
+    address_2
+    city
+    region
+    postal_code
+    country
+    day_phone
+    eve_phone
+    mob_phone
+  end
+end
